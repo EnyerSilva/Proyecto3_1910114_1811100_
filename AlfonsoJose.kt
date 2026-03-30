e24dc0bbbc4e48e73d8fe3ce85767b259d666594
Enyerber Silva <18-11100@usb.ve>
2026-03-30 17:34:47 -0400

    Update AlfonsoJose.kt

--- AlfonsoJose.kt
+++ AlfonsoJose.kt
@@ -3,5 +3,26 @@ data class NodoTorre(val fila: Int, val col: Int, val altura: Int) : Comparable<
 }
 
 class AlfonsoJose{
-    
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val nombreArchivo = "atlantis.txt"
+            val archivo = File(nombreArchivo)
+
+            if (!archivo.exists()) return
+
+            val lineas = archivo.readLines()
+            if (lines.isEmpty()) return
+
+            val n = lineas.size
+            val m = lines[0].length
+            val matriz = Array(n) { i -> lineas[i].map { it.toString().toInt() } .toIntArray()
+            }
+
+            println(calcularAguaAtrapada(matriz, n, m))
+        }
+
+        
+
+    }
 }
