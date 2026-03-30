8d7bf9122ec830725cc6cbd25069d28be4830be0
Enyerber Silva <18-11100@usb.ve>
2026-03-30 17:27:11 -0400

    Update AlfonsoJose.kt

--- AlfonsoJose.kt
+++ AlfonsoJose.kt
@@ -1,3 +1,7 @@
 data class NodoTorre(val fila: Int, val col: Int, val altura: Int) : Comparable<NodoTorre> {
     override fun compareTo(other: NodoTorre): Int = this.altura.compareTo(other.altura)
 }
+
+class AlfonsoJose{
+    
+}
